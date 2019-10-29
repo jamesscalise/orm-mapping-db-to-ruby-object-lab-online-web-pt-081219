de96e9c4a2525ba307cd1ee6d024fdb609426e42
James Scalise <jamescscalise@gmail.com>
2019-10-29 05:01:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -94,6 +94,17 @@ class Student
   end
   
   def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+      ORDER BY students.id ASC
+    SQL
+    
+    
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
   end
   
   def save
